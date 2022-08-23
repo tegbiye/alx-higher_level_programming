4d86d6fdad04a3a22cceb8c3a8636230aae4f1bd
tegbiye <se.tegbabu.solomon@gmail.com>
2022-08-23 15:53:05 +0300

    Fixed error in task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("0{}".format(i), end=", ")
-    elif 10 < i < 99:
-        print("{}".format(i), end=", ")
+    if i == 99:
+        print(i)
     else:
-        print("{}".format(i), end=" ")
-print()
+        print("{:0>2d}".format(i), end=", ")
