c9dc540988f6d2385dca123f956f1e4b3ae928a5
tegbiye <se.tegbabu.solomon@gmail.com>
2022-08-26 13:54:26 +0300

    Fixed error

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # 102-magic_calculation.py
 
-def magic_calculation(a, b, c):
+def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
