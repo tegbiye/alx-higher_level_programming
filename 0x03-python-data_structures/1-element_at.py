51fd1c0a292ac937a412642963b0611da8b88b9b
tegbiye <se.tegbabu.solomon@gmail.com>
2022-08-29 12:40:44 +0300

    Fixed error in task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,6 @@
 # 1-element_at.py
 
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    if idx > len(my_list):
+    if idx < 0 or idx > (len(my_list) - 1):
         return None
     return my_list[idx]
