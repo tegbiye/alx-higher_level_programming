3528b5dadb8d8a2ec255f0871df4d13cd1ded974
tegbiye <se.tegbabu.solomon@gmail.com>
2022-10-01 01:25:59 +0300

    Added task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Import json module
+'''
+
+
+import json
+'''
+from json string
+'''
+
+
+def from_json_string(my_str):
+    '''
+    from json string
+    '''
+    return json.loads(my_str)
