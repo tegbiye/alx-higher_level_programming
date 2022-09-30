1e5bf2d81558bfc889afd076700117405851d982
tegbiye <se.tegbabu.solomon@gmail.com>
2022-10-01 01:42:47 +0300

    Fixed error in task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,9 +13,17 @@ Importing funcs
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+'''
+Declaration
+'''
+
 
 filename = "add_item.json"
 json_list = []
+'''
+load, append, save
+'''
+
 
 if os.path.exists(filename):
     json_list = load_from_json_file(filename)
