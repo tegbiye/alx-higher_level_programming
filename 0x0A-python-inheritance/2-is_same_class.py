ca16b547d6bcf25d956972eaf7d54e64e760d551
tegbiye <se.tegbabu.solomon@gmail.com>
2022-09-30 23:28:03 +0300

    Fixed error in task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,4 +7,4 @@ module with method is_same_class
 def is_same_class(obj, a_class):
     """Method that return True if an object is an instance of a class"""
 
-    return type(obj) is
+    return type(obj) is a_class
