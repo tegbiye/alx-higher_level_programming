13a3ea3100ed7550d9295af7f6a1ea63da106083
tegbiye <se.tegbabu.solomon@gmail.com>
2022-11-01 22:27:54 +0300

    Fixed error in task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 --lists all records of the second_table of database.
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT `score`, `name` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC;
