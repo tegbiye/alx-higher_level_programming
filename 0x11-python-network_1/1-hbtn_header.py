7b4da6f8f0f7157115ff0f0f50e0201b9fb70189
tegbiye <se.tegbabu.solomon@gmail.com>
2022-12-16 23:27:15 +0300

    Added task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Fetches header"""
+import urllib.request
+import sys
+
+
+def fetcher():
+    """fetcher"""
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        header = response.info()
+        print(header["X-Request-Id"])
+
+
+if __name__ == "__main__":
+    fetcher()
