d1e6c8ead97710dc2c1ab3cb1f58884e78e1305f
tegbiye <se.tegbabu.solomon@gmail.com>
2022-12-17 12:48:50 -0800

    Added task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Check status"""
+import requests
+import sys
+
+
+def header():
+    """status"""
+    result = requests.get(sys.argv[1])
+
+    print(result.headers.get("X-Request-Id", None))
+
+
+if __name__ == "__main__":
+    header()
