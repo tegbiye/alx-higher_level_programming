1723988b3898ac9722d8b14e6d8f5255080e066d
tegbiye <se.tegbabu.solomon@gmail.com>
2024-02-20 14:27:57 +0300

    Added task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Importing module
+'''
+
+
+import json
+'''
+To Json string
+'''
+
+
+def to_json_string(my_obj):
+    '''
+    To Json string
+    '''
+    return json.dumps(my_obj)
