1e8a6a961e8f168b2089b021ee22c7e8add205a3
tegbiye <se.tegbabu.solomon@gmail.com>
2024-05-10 05:41:32 -0700

    fixed error in task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,9 @@
 ''' script that fetches https://intranet.hbtn.io/status '''
 import urllib.request
 
-
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-        html = res.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
